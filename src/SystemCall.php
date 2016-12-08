684aedce24771f67b661def025009e191a5a47a5
Wu Weixin <wuweixin@gmail.com>
2016-12-08 14:46:10 +0800

    SystemCall class

---
+++ src/SystemCall.php
@@ -0,0 +1,15 @@
+<?php namespace Coroutine;
+
+class SystemCall
+{
+    protected $callback;
+
+    public function __construct(callable $callback) {
+        $this->callback = $callback;
+    }
+
+    public function __invoke(Task $task, Scheduler $scheduler) {
+        $callback = $this->callback;
+        return $callback($task, $scheduler);
+    }
+}
